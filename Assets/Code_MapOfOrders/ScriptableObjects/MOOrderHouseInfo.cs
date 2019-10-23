0e10481651cf1bbee3b96d08ddf09ffd3966df6e
vardaren <vardaren@empyrean.games>
2019-10-23 12:42:12 +0200

    Load house info into UI

--- Assets/Code_MapOfOrders/ScriptableObjects/MOOrderHouseInfo.cs
+++ Assets/Code_MapOfOrders/ScriptableObjects/MOOrderHouseInfo.cs
@@ -3,6 +3,10 @@
 namespace HGTV.MapsOfOrders {
     [CreateAssetMenu(menuName = "House Flipper/HGTV/Map of orders setups/Create Order House")]
     public class MOOrderHouseInfo : ScriptableObject {
+        public string budget;
+        public int choices;
+        public int areas;
+        public string title;
         public string details;
     }
 }
---
+++ Assets/Code_MapOfOrders/UI/MapView.cs
@@ -0,0 +1,24 @@
+﻿using HGTV.MapsOfOrders;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Code_MapOfOrders.UI {
+    public class MapView : MonoBehaviour {
+        [SerializeField] InfoPanel infoPanel;
+        [SerializeField] NotePanel notePanel;
+        [SerializeField] Button playButton;
+
+        public void LoadData(MOOrderHouseInfo mOMapOrderInfo) {
+            infoPanel.LoadData(mOMapOrderInfo.title, mOMapOrderInfo.details);
+            notePanel.LoadData(mOMapOrderInfo.budget, mOMapOrderInfo.choices, mOMapOrderInfo.areas);
+            playButton.onClick.AddListener(delegate { Play(mOMapOrderInfo.title); });
+        }
+
+        void Play(string title) {
+            Debug.Log("PLAYING " + title);
+        }
+    }
+}
